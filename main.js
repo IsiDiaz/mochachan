77cdeb938bc82a34581de8980b0c827a4f6125ad
IsiDiaz <142444818+IsiDiaz@users.noreply.github.com>
2024-09-30 09:34:28 -0300

    gr

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
-// alert('Isis Diaz e Luanne Bermirio')
-function soma(num1, num2){
-return num1+num2;
-}
-soma (2,3);
-console.log (soma(2,3));
-function area(lado){
-    return  lado**2;
-}
-console.log (area(6));
+// // alert('Isis Diaz e Luanne Bermirio')
+// function soma(num1, num2){
+// return num1+num2;
+// }
+// soma (2,3);
+// console.log (soma(2,3));
+// function area(lado){
+//     return  lado**2;
+// }
+// console.log (area(6));
 
-let areaTriangulo = function(base,altura){
-    return (base*altura)/2;
-}
-console.log (areaTriangulo(2,3))
+// let areaTriangulo = function(base,altura){
+//     return (base*altura)/2;
+// }
+// console.log (areaTriangulo(2,3))
 
-let nome = parseInt (prompt('Quanto é 2+2?'));
-let nome = parseInt (prompt('Quanto é 2+8?'));
-let nome = parseInt (prompt('Quanto é 2+8?'));
-document.write(4);
-document.write(10);
-document.write(10);
+// let nome = parseInt (prompt('Quanto é 2+2?'));
+// let nome = parseInt (prompt('Quanto é 2+8?'));
+// let nome = parseInt (prompt('Quanto é 2+8?'));
+// document.write(4);
+// document.write(10);
+// document.write(10);
 
 let estados = [
     'Sergipe',
@@ -49,4 +49,4 @@ let estados = [
     'Goiás',
     'Distrito Federal',
     'Maranhão'
-];
+]
